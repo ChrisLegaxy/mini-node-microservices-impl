dda56278b57b313315e2ee90a4d89fa415381f14
Chris Legaxy <chris_legaxy@yahoo.com>
2021-04-11 23:35:31 +0700

    refactor(events): comments and minor changes

--- event-bus/index.ts
+++ event-bus/index.ts
@@ -1,21 +1,41 @@
+/**
+ * * Imports
+ */
 import express, { Request, Response } from "express";
 import axios from "axios";
 
-const POST_BASE_URL = "http://localhost:4000";
+/**
+ * * Constants
+ */
+const POSTS_BASE_URL = "http://localhost:4000/";
+const COMMENTS_BASE_URL = "http://localhost:5000";
 
+/**
+ * * Data
+ */
+import { events } from "./data/events";
+
+/**
+ * * Initialize server
+ */
 const server = express();
 
+/**
+ * * Middlewares
+ */
 server.use(express.urlencoded({ extended: true }));
 server.use(express.json());
 
-const events: any[] = [];
-
+/**
+ * * Routes & Controllers
+ */
 server.post("/events", (request: Request, response: Response) => {
   const event = request.body;
 
   events.push(event);
 
-  axios.post(`${POST_BASE_URL}/events`, event);
+  axios.post(`${POSTS_BASE_URL}/events`, event);
+  axios.post(`${COMMENTS_BASE_URL}/events`, event);
 
   return response.json({
     message: "OKAY",
@@ -24,7 +44,7 @@ server.post("/events", (request: Request, response: Response) => {
 
 server.get("/events", (request: Request, response: Response) => {
   return response.json(events);
-})
+});
 
 server.listen(5000, () => {
   console.log("Server is listing on PORT 5000");
