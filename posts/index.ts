45d9574c3ccb44d84d51800a687af2fb90f5767e
Chris Legaxy <chris_legaxy@yahoo.com>
2021-04-11 23:23:03 +0700

    feat(posts): add necessary implementation

---
+++ posts/index.ts
@@ -0,0 +1,74 @@
+/**
+ * * Imports
+ */
+import express, { Request, Response } from "express";
+import axios from "axios";
+import { v4 as uuidv4 } from "uuid";
+
+/**
+ * * Data
+ */
+import { posts } from "./data/posts";
+
+/**
+ * * Constants
+ */
+const EVENT_BUS_URL = "http://localhost:5000/events";
+
+/**
+ * * Initiaize server
+ */
+const server = express();
+
+/**
+ * * Middlewares
+ */
+server.use(express.urlencoded({ extended: true }));
+server.use(express.json());
+
+/**
+ * * Routes
+ */
+server.get("/posts", (request: Request, response: Response) => {
+  return response.json(posts);
+});
+
+server.post("/posts", async (request: Request, response: Response) => {
+  const id = uuidv4();
+  const { title } = request.body;
+
+  posts[id] = {
+    id,
+    title,
+  };
+
+  /* emit 'PostCreated' event */
+  try {
+    await axios.post(EVENT_BUS_URL, {
+      type: "PostCreated",
+      data: posts[id],
+    });
+  } catch(error) {
+    return response.status(500).json({
+      message: "Something went wrong"
+    })
+  }
+
+  return response.json(posts[id]);
+});
+
+/**
+ * * Recieving Events
+ */
+server.post("/events", (request: Request, resposne: Response) => {
+  console.log("=============================================");
+  console.log("Event recieved:", request.body.type);
+  console.log("Data:", request.body.data);
+  console.log("=============================================");
+
+  return resposne.end();
+});
+
+server.listen(4000, () => {
+  console.log("Server is running on PORT 4000");
+});
