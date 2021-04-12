36905ce03a1e785e0c160b38dec614f47ecae5c3
Chris Legaxy <chris_legaxy@yahoo.com>
2021-04-12 16:06:20 +0700

    feat(comments): add event handling for comment moderated event and minor changes

--- comments/methods/event-handling.ts
+++ comments/methods/event-handling.ts
@@ -1,17 +1,52 @@
+import axios from "axios";
 import { commentsByPostId } from "../data/comments-by-post-id";
 
-export const handleEvent = (eventType: string, eventData: any) => {
+export const handleEvent = async (
+  eventType: string,
+  eventData: any,
+  EVENT_BUS_URL: string
+) => {
   switch (eventType) {
     case "PostCreated": {
-      if (!commentsByPostId[eventData.id]) {
-        commentsByPostId[eventData.id] = []
+      const post = eventData;
+
+      if (!commentsByPostId[post.id]) {
+        commentsByPostId[post.id] = [];
       }
+
+      break;
+    }
+    case "CommentModerated": {
+      /**
+       * * comment
+       */
+      const { postId, id, status } = eventData;
+
+      if (commentsByPostId[postId]) {
+        const comments = commentsByPostId[postId];
+
+        // comment to be updated after moderation
+        const comment = comments.find((comment) => comment.id === id);
+
+        if (comment) {
+          comment.status = status;
+        }
+
+        await axios.post(EVENT_BUS_URL, {
+          type: "CommentUpdated",
+          data: {
+            postId,
+            ...comment,
+          },
+        });
+      }
+
       break;
     }
     default: {
-      break
+      break;
     }
   }
 
   return;
-}
+};
